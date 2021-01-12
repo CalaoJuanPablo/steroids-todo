5f3897bfef9d237dd92a4c3852f6b491f26fdd8a
Juan Pablo Calao <jcalaop@gmail.com>
2021-01-12 12:09:13 -0500

    add capitalize string util

--- src/templates/ToDos/index.tsx
+++ src/templates/ToDos/index.tsx
@@ -6,21 +6,21 @@ import { useRouter } from 'next/router'
 // Components
 import MainLayout from '../../layouts/Main'
 import { getToDoState } from '../../utils/getToDoState'
+import { capitalizeString } from '../../utils/capitalizeString'
 
 function ToDosTemplate(): ReactElement {
   const { pathname } = useRouter()
 
   const toDoState = getToDoState(pathname)
+  const capitalToDoState = toDoState !== null ? capitalizeString(toDoState) : ''
 
   return (
     <>
       <Head>
-        <title>Steroids ToDo | All ToDos</title>
+        <title>Steroids ToDo | {capitalToDoState} ToDos</title>
       </Head>
       <MainLayout>
-        <h1 className='f-headline lh-solid'>
-          {toDoState?.toUpperCase()} ToDos Page
-        </h1>
+        <h1 className='f-headline lh-solid'>{capitalToDoState} ToDos Page</h1>
       </MainLayout>
     </>
   )
