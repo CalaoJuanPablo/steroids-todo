88d9a7ed5765e2c435ccde60008ac36b2f11966e
Juan Pablo Calao <jcalaop@gmail.com>
2021-01-12 18:24:16 -0500

    center footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from 'react'
 function Footer(): ReactElement {
   return (
     <footer className='w-100 pa4'>
-      <p className='w-100 mw8 tc'>
+      <p className='w-100 mw8 center tc'>
         Made with ❤️ and &lt; / &gt; using <code>Javascript</code>
       </p>
     </footer>
